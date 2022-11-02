d9d9f3b72ac83b0d10de46c68e356d36e333b0e3
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-02 12:11:22 -0400

    small change

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,9 @@
 # Basic usage of key-value pairs.
 # for each number the correct compliment to the target can be easily found.
-# knowing that the current index contains the correct summand a corresponding
+# knowing that the current index contains the correct summand for a corresponding
 # compliment, we can store a key-value pair {compliment : current-index} so
-# that if that compliment is seen in a future iteration we can return that
-# current index and the stored index
+# that if that specific compliment is seen in a future iteration we can return the
+# index of that iteration and the stored index.
 
 def twoSum(self, nums, target):
     d = {}
