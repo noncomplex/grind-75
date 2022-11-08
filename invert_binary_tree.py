dbbbf21fb331aff1f366f0d6d3f0d2184989c306
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-08 14:33:20 -0500

    valid anagram

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -1,8 +1,22 @@
-# simply recursively swap
-# left = right and right = left
+# use a dictionary to determine the counts for each char in 1 str
+# and use the other str to decrement each occurence
+# if every element has a count of 0 then we know it's an anagram
+
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root:
-            return TreeNode(root.val,
-                            self.invertTree(root.right),
-                            self.invertTree(root.left))
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        d = {}
+        for i in range(len(s)):
+            if(s[i] in d):
+                d[s[i]] += 1
+            elif(s[i] not in d):
+                d[s[i]] = 1
+            if(t[i] in d):
+                d[t[i]] -= 1
+            elif(t[i] not in d):
+                d[t[i]] = -1
+        for c in d:
+            if d[c] != 0:
+                return False
+        return True
