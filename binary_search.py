2fe106b049fe49def38b665068e2a808b22e4103
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-12 22:21:16 -0500

    binary search

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+# sorted list so can just discard right of mid when < and left of mid when > then
+# affirm the +-1 logic using examples with list of 2 numbers
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
