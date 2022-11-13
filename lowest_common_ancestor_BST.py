2072bc6d89f31a82ed3ee59e51f6f63ac8172653
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-12 21:26:53 -0500

    lca bst

---
+++ lowest_common_ancestor_BST.py
@@ -0,0 +1,12 @@
+# given the constraints, can go left and right down the tree
+# until p and q are descendants of the current node
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
