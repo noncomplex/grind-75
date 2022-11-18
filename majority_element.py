5d4fb1376f4cd0526153ce67b8ea544e8752959a
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-18 17:22:14 -0500

    majority element follow up

--- majority_element.py
+++ majority_element.py
@@ -10,3 +10,15 @@ class Solution:
             maxc = max(counts[n], maxc)
             maxn = n if counts[n] == maxc else maxn
         return maxn
+
+    # follow-up O(1) space
+    # can just do +1 or -1 on each character because
+    # and update on +1 because the majority will always be at
+    # a minimum +1 count of any other number
+    def majorityElement2(self, nums: List[int]) -> int:
+        counts, maxn = 1, 0
+        for n in nums:
+            if counts == 1:
+                maxn = n
+            counts += (1 if n == maxn else -1)
+        return maxn
