4779762a338739e1f76f12a4c66f81c95268fd44
noncomplex <71818162+noncomplex@users.noreply.github.com>
2022-11-19 20:05:43 -0500

    max depth binary tree

---
+++ max_depth_bt.py
@@ -0,0 +1,8 @@
+# simple recursive solution;
+# add 1 and recursively compare at each step
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
